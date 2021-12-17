ceec5588145c952d19e201a1b8514d8aa8ef45a6
Michelle <mchristine.stone91@gmail.com>
2021-12-17 00:23:04 -0500

    wrote getStats and getInventory

--- _tests_/Player.test.js
+++ _tests_/Player.test.js
@@ -4,8 +4,6 @@ const Potion = require("../lib/Potion");
 
 jest.mock("../lib/Potion");
 
-console.log(new Potion());
-
 //Want players to have a name and three number properties: health, strength, agility
 //Test that checks for existence of those things:
 test("creates a player object", () => {
@@ -15,7 +13,27 @@ test("creates a player object", () => {
   expect(player.health).toEqual(expect.any(Number));
   expect(player.strength).toEqual(expect.any(Number));
   expect(player.agility).toEqual(expect.any(Number));
+
   expect(player.inventory).toEqual(
     expect.arrayContaining([expect.any(Object)])
   );
+});
+
+test("gets player's stats as an object", () => {
+  const player = new Player("Dave");
+
+  expect(player.getStats()).toHaveProperty("potions");
+  expect(player.getStats()).toHaveProperty("health");
+  expect(player.getStats()).toHaveProperty("strength");
+  expect(player.getStats()).toHaveProperty("agility");
+});
+
+test("gets inventory from player or returns false", () => {
+  const player = new Player("Dave");
+
+  expect(player.getInventory()).toEqual(expect.any(Array));
+
+  player.inventory = [];
+
+  expect(player.getInventory()).toEqual(false);
 });

5fc58fd0ac1c6638acf9b0ebaee80517a0f01355
Michelle <mchristine.stone91@gmail.com>
2021-12-17 20:23:01 -0500

    wrote player attack and potion methods
    da
    '

--- _tests_/Player.test.js
+++ _tests_/Player.test.js
@@ -69,3 +69,34 @@ test("subtracts from player's health", () => {
 
   expect(player.health).toBe(0);
 });
+
+//Test to get player's attack value
+test("get player's attack value", () => {
+  const player = new Player("Dave");
+  player.strength = 10;
+
+  expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+  expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+//Check that potion was added correctly
+test("adds a potion to the inventory", () => {
+  const player = new Player("Dave");
+  const oldCount = player.inventory.length;
+
+  player.addPotion(new Potion());
+
+  expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+//Ensure that usePotion removes correct potion from the inventory
+//Will use index of potion to keep track which one has been selected
+test("uses a potion from inventory", () => {
+  const player = new Player("Dave");
+  player.inventory = [new Potion(), new Potion(), new Potion()];
+  const oldCount = player.inventory.length;
+
+  player.usePotion(1);
+
+  expect(player.inventory.length).toBeLessThan(oldCount);
+});
